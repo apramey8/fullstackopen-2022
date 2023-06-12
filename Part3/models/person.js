7c96f2be669de39ce65e9783c905c07706cb275c
Apramey <apramey.bhat@aalto.fi>
2023-06-13 00:53:05 +0300

    Exercise 3.19-3.21 (Validations)

--- Part3/models/person.js
+++ Part3/models/person.js
@@ -20,6 +20,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    validate: {
+      validator: function(v) {
+        return /^(\d{2}|\d{3})-\d+/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
     minLength: 8,
     required: true
   },

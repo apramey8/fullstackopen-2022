69c24bc2a29266312e424de36a1c8627cfe25639
Apramey <apramey.bhat@aalto.fi>
2023-06-13 23:05:46 +0300

    Exe - 4.6*: helper functions and unit tests, step4

--- Part4/utils/listHelper.js
+++ Part4/utils/listHelper.js
@@ -1,23 +1,38 @@
 const dummy = (blogs) => {
-    return 1;
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const mostLiked = blogs.reduce((previous, current) => {
+    return previous.likes > current.likes ? previous : current;
+  });
+
+  return {
+    title: mostLiked.title,
+    author: mostLiked.author,
+    likes: mostLiked.likes,
   };
-  
-  const totalLikes = (blogs) => {
-    return blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const lodash = require("lodash");
+
+const mostBlogs = (blogs) => {
+  const blogCount = lodash.countBy(blogs, "author");
+
+  const maxAuthor = Object.keys(blogCount).reduce((a, b) => {
+    return blogCount[a] > blogCount[b] ? a : b;
+  });
+
+  return {
+    author: maxAuthor,
+    blogs: blogCount[maxAuthor],
   };
-  
-  const favoriteBlog = (blogs) => {
-    if (blogs.length === 0) return null;
-  
-    const mostLiked = blogs.reduce((previous, current) => {
-      return previous.likes > current.likes ? previous : current;
-    });
-  
-    return {
-      title: mostLiked.title,
-      author: mostLiked.author,
-      likes: mostLiked.likes,
-    };
-  };
-  
-  module.exports = { dummy, totalLikes, favoriteBlog };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };

5b3d438157a2fdddf53179baaa18b32ccb256c39
Apramey <apramey.bhat@aalto.fi>
2023-06-13 22:14:21 +0300

    Added config.js in utils

--- Part4/index.js
+++ Part4/index.js
@@ -1,9 +1,7 @@
-const http = require("http");
-const express = require("express");
-const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
+const config = require("./utils/config");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -14,16 +12,15 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
 mongoose
-  .connect(mongoUrl)
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info("connected to MongoDB");
   })
   .catch((error) => {
     logger.error("error connecting to MongoDB:", error.message);
   });
-
+  
 app.use(cors());
 app.use(express.json());
 
@@ -40,8 +37,8 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
+const PORT = config.PORT;
 
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });

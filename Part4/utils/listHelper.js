08115126091dbcef83f3e73eb8c48ec0112567d4
Apramey <apramey.bhat@aalto.fi>
2023-06-13 22:58:05 +0300

    Exe 4.5*: helper functions and unit tests, step3

--- Part4/utils/listHelper.js
+++ Part4/utils/listHelper.js
@@ -3,9 +3,21 @@ const dummy = (blogs) => {
   };
   
   const totalLikes = (blogs) => {
-    return blogs.length === 0
-      ? 0
-      : blogs.reduce((sum, post) => sum + post.likes, 0);
+    return blogs.length === 0 ? 0 : blogs.reduce((sum, blog) => sum + blog.likes, 0);
   };
   
-  module.exports = { dummy, totalLikes };
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+  
+    const mostLiked = blogs.reduce((previous, current) => {
+      return previous.likes > current.likes ? previous : current;
+    });
+  
+    return {
+      title: mostLiked.title,
+      author: mostLiked.author,
+      likes: mostLiked.likes,
+    };
+  };
+  
+  module.exports = { dummy, totalLikes, favoriteBlog };

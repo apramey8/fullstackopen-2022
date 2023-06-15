3c7614c6790e6fb275e7a6bd576fd4c37fb26f7f
Apramey <apramey.bhat@aalto.fi>
2023-06-15 23:36:38 +0300

    Exercises 4.13.-4.14.

--- Part4/controllers/blogs.js
+++ Part4/controllers/blogs.js
@@ -5,12 +5,26 @@ blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
-
 blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
-
   const savedBlog = await blog.save();
   response.status(201).json(savedBlog.toJSON());
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const likes = request.body;
+  const id = request.params.id;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, likes, { new: true });
+
+  updatedBlog
+    ? response.status(200).json(updatedBlog.toJSON())
+    : response.status(404).end();
 });
 
 module.exports = blogsRouter;
